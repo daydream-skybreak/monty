313d410333202cbfab50e639bef2c665dd745f69
Samuel <samueldesta323@gmail.com>
2022-07-14 10:22:26 +0300

    functions for push pall and pint

---
+++ operation_1.c
@@ -0,0 +1,67 @@
+#include "monty.h"
+
+/**
+ * instruction_push - pushes an element to the stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ * Return: nothing
+ */
+void instruction_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = malloc(sizeof(stack_t));
+	char *op;
+	int num;
+
+	if (element == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	op = strtok(NULL, DELIMS);
+	if (op == NULL || stack == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	num = _strtol(op, line_number);
+	element->n = num;
+	element->prev = NULL;
+	element->next = *stack;
+	if (element->next != NULL)
+		(element->next)->prev = element;
+	*stack = element;
+}
+
+/**
+ * instruction_pall - prints all values on the stack, starting from top
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ * Return: nothing
+ */
+void instruction_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *element = *stack;
+
+	UNUSED(line_number);
+	while (element != NULL)
+	{
+		printf("%d\n", element->n);
+		element = element->next;
+	}
+}
+
+/**
+ * instruction_pint - prints value at top of stack
+ * @stack: double pointer to the first node
+ * @line_number: value of new node
+ * Return: nothing
+ */
+void instruction_pint(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
